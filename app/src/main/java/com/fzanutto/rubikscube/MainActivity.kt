eeb195cf8341602f07ec991aeda1efd4db1c3b81
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-11-20 18:57:43 -0300

    Initial commit

---
+++ app/src/main/java/com/fzanutto/rubikscube/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fzanutto.rubikscube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cube.Cube
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
